1943de65b507864cd1b946f28d85730dd143ad32
OleLundg <90825627+OleLundg@users.noreply.github.com>
2022-12-09 10:32:35 +0100

    Update api_client.py
    bug fix

--- api_client.py
+++ api_client.py
@@ -3,14 +3,14 @@ import json
 import time
 
 def get_weather():
-    weather_url = 'http://127.0.0.1:5000/weather_api/v1/'
+    weather_url = 'http://127.0.0.1:9080/weather_api/v1/'
     response = urllib.request.urlopen(weather_url)
     weather_topics = response.read()
     json_topics = json.loads(weather_topics)
     print(json_topics)
 
 def get_choice(choice):
-    choice_url = f'http://127.0.0.1:5000/weather_api/v1/{choice}'
+    choice_url = f'http://127.0.0.1:9080/weather_api/v1/{choice}'
     response = urllib.request.urlopen(choice_url)
     hum_temp = response.read()
     json_hum_temp = json.loads(hum_temp)
@@ -18,14 +18,14 @@ def get_choice(choice):
 
 if __name__ == "__main__":
     main_loop = True
-    while main_loop == True:
+    while main_loop:
         choice = input('Fetch:\n' \
                    '1. All data\n' \
                    '2. Temperature data\n' \
                    '3. Humidity data\n' \
                    'To exit type "exit"\n')
         loop = True
-        while (loop==True):
+        while loop:
             time.sleep(1)
             if choice == '1':
                 main_loop = False
@@ -38,9 +38,8 @@ if __name__ == "__main__":
                 elif fetching_choice == '1':
                     continue
             if choice == '2':
-                main_loop = False
-                choice = 'temp'
-                get_choice(choice)
+                main_loop = False               
+                get_choice('temp')
                 fetching_choice = input('1. Continue \n'
                                         '2. Back to main menu \n')
                 if fetching_choice == '2':
@@ -50,8 +49,7 @@ if __name__ == "__main__":
                     continue
             if choice == '3':
                 main_loop = False
-                choice = 'humidity'
-                get_choice(choice)
+                get_choice('humidity')
                 fetching_choice = input('1. Continue \n'
                                         '2. Back to main menu \n')
                 if fetching_choice == '2':
